c71d4433689a7b5006587b97858d38a4e079c9b1
Fernando <fernando_lopezc@live.com>
2020-03-26 11:21:20 +0000

    Add interface for post

---
+++ src/blog/post.interface.ts
@@ -0,0 +1,9 @@
+import { document } from 'mongoose'
+
+interface Post extends Document {
+    readonly title: string;
+    readonly description:  string;
+    readonly body: string;
+    readonly author: string;
+    readonly date_posted: string; 
+}
