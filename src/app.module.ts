6123131f15d57697fc760c002f56be83ac770d10
Fernando <fernando_lopezc@live.com>
2020-03-26 10:55:11 +0000

    Set up datacase connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
+// Here, you imported the MongooseModule into the root AppModule and then used the forRoot() method to supply the connection to the database
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest-blog-project', { useNewUrlParser: true }),],
   controllers: [AppController],
   providers: [AppService],
 })
